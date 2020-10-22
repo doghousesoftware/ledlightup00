2b2c6a734beb882469024b8c6fb7a14e2824c955
bryan weinstein <bryanweinstein@outlook.com>
2020-10-21 17:42:37 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
-input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-})
+let poop = 0
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(input.lightLevel())
-})
-input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    for (let index = 0; index <= 1000; index++) {
+        pins.analogWritePin(AnalogPin.P0, index)
+        basic.pause(100)
+    }
+    poop = 1000
+    for (let index = 0; index <= 999; index++) {
+        pins.analogWritePin(AnalogPin.P0, poop)
+        basic.pause(100)
+        poop += -1
+    }
 })
