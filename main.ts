8c7cff486a45be336690a9ad18a2baa5a735f267
bryan weinstein <bryanweinstein@outlook.com>
2020-10-21 17:43:44 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
 let poop = 0
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index <= 1000; index++) {
-        pins.analogWritePin(AnalogPin.P0, index)
+        pins.analogWritePin(AnalogPin.P1, index)
         basic.pause(100)
     }
     poop = 1000
     for (let index = 0; index <= 999; index++) {
-        pins.analogWritePin(AnalogPin.P0, poop)
+        pins.analogWritePin(AnalogPin.P1, poop)
         basic.pause(100)
         poop += -1
     }
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
