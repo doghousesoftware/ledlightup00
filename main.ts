17886071c483b64aa9137680ac04d4e1817fdbb2
bryan weinstein <bryanweinstein@outlook.com>
2021-02-17 15:29:19 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,54 @@
-let poop = 0
-input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-})
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index <= 1000; index++) {
-        pins.analogWritePin(AnalogPin.P1, index)
-        basic.pause(100)
+let iCounter = 0
+let oCounter = 0
+input.onPinPressed(TouchPin.P0, function () {
+    iCounter = 4
+    basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        oCounter = 4
+        for (let index = 0; index < 5; index++) {
+            led.toggle(oCounter, iCounter)
+            basic.pause(200)
+            oCounter += -1
+        }
+        iCounter += -1
     }
-    poop = 1000
-    for (let index = 0; index <= 999; index++) {
-        pins.analogWritePin(AnalogPin.P1, poop)
-        basic.pause(100)
-        poop += -1
+})
+input.onButtonPressed(Button.A, function () {
+    oCounter = 0
+    basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        iCounter = 0
+        for (let index = 0; index < 5; index++) {
+            led.toggle(oCounter, iCounter)
+            basic.pause(200)
+            iCounter += 1
+        }
+        oCounter += 1
     }
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    oCounter = 4
+    basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        iCounter = 4
+        for (let index = 0; index < 5; index++) {
+            led.toggle(oCounter, iCounter)
+            basic.pause(200)
+            iCounter += -1
+        }
+        oCounter += -1
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    iCounter = 0
+    basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        oCounter = 0
+        for (let index = 0; index < 5; index++) {
+            led.toggle(oCounter, iCounter)
+            basic.pause(200)
+            oCounter += 1
+        }
+        iCounter += 1
+    }
 })
